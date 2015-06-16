1d2a16a86a4d01878ac11ef9d5dc92a9eb0e73cd
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-06-16 15:54:01 +0200

    Update fusion make file.
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- fusionsp.mk
+++ fusionsp.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/fusion/config/common_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := fusionsp_vs985
+PRODUCT_NAME := vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
